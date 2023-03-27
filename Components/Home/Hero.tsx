96fab40bd685b33a9ed2fe40f76052e83e5cc88d
unknown <alaminkhan6203@gmail.com>
2023-03-27 23:45:28 +0600

    css fixed

--- Components/Home/Hero.tsx
+++ Components/Home/Hero.tsx
@@ -10,7 +10,7 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div className="flex flex-col bg-white lg:grid lg:grid-cols-2 justify-center items-center gap-10 px-10 pt-10 lg:pt-0">
+    <div className="flex flex-col bg-white lg:grid lg:grid-cols-2 justify-center items-center gap-10 px-10 pt-10 lg:pt-4">
       <div>
         <p className="text-4xl font-bold text-black">
           <span className="bg-indigo-100 p-2 rounded-md">Power</span> Your Ride!
@@ -24,36 +24,46 @@ const Hero = (props: Props) => {
           assured, we&apos;ll continue to set the standard for the industry with
           our cutting-edge technology and superior products.
         </p>
-        <div className="flex justify-center gap-2 mt-4">
-          <IoCheckmarkDoneCircle className="w-8 h-8 text-indigo-500 " />
-          <p>
-            {" "}
-            <span className="font-bold text-black">Insure Best Quality</span> –
-            components are manufactured to the strictest tolerances.
-          </p>
+        <div className="flex flex-col ">
+          <div className="flex  gap-2 mt-4">
+            <div className="">
+              <IoCheckmarkDoneCircle className=" w-10 text-indigo-500 " />
+            </div>
+            <p>
+              {" "}
+              <span className="font-bold text-black">
+                Insure Best Quality
+              </span>{" "}
+              – components are manufactured to the strictest tolerances.
+            </p>
+          </div>
+          <div className="flex  gap-2 mt-2">
+            <div className="">
+              <IoCheckmarkDoneCircle className=" w-10 text-indigo-500 " />
+            </div>
+            <p>
+              {" "}
+              <span className="font-bold text-black">Deliver Best</span> – We
+              deliver top quality injection molded parts produced by our IATF
+              16949 certified factory in Poland.
+            </p>
+          </div>
+          <div className="flex gap-2 mt-2">
+            <div className="">
+              <IoCheckmarkDoneCircle className=" w-10 text-indigo-500 " />
+            </div>
+            <p>
+              {" "}
+              <span className="font-bold text-black">100% recyclable </span> –
+              All of our products are 100% recyclable
+            </p>
+          </div>
         </div>
-        <div className="flex justify-center gap-2 mt-2">
-          <IoCheckmarkDoneCircle className="w-12 h-12 text-indigo-500 " />
-          <p>
-            {" "}
-            <span className="font-bold text-black">Deliver Best</span> – We
-            deliver top quality injection molded parts produced by our IATF
-            16949 certified factory in Poland.
-          </p>
-        </div>
-        <div className="flex justify-center gap-2 mt-2">
-          <IoCheckmarkDoneCircle className="w-[25px] h-[25px] text-indigo-500 " />
-          <p>
-            {" "}
-            <span className="font-bold text-black">100% recyclable </span> – All
-            of our products are 100% recyclable
-          </p>
-        </div>
-        <div className="mt-4">
+        <div className="mt-4 w-[150px]">
           <Link
             href="https://github.com/AlAminKh03/motor-merchants"
             target="_blank"
-            className="flex items-center justify-center gap-6 border border-indigo-500 bg-indigo-50 hover:bg-indigo-100 transition-all ease-in duration-300 mr-[40%] lg:mr-[64%] p-2  rounded-md"
+            className="flex items-center justify-start gap-6 border  border-indigo-500 bg-indigo-50 hover:bg-indigo-100 transition-all ease-in duration-300  p-2  rounded-md"
           >
             <AiFillGithub className="w-6 h-6" />
             <p className="font-bold">Source Code</p>
