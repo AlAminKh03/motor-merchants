229a6ce9c23a4761e6762c1fe73edcf78f68281d
AlAminKh03 <alaminkhan6203@gmail.com>
2023-09-27 15:27:15 +0600

    review design changed

--- Components/Home/Reviews.tsx
+++ Components/Home/Reviews.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { CgProfile } from "react-icons/cg";
-import { BsChatLeft } from "react-icons/bs";
-import Slider from "react-slick";
+import star from "../../public/star.json";
 import { FaStar } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay } from "swiper/modules";
+import Lottie from "lottie-react";
 
 type Props = {};
 interface ReviewProps {
@@ -32,98 +38,87 @@ const Reviews = (props: Props) => {
     const stars = [];
     for (let i = 0; i < 5; i++) {
       if (i < rating) {
-        if (rating <= 1) {
-          stars.push(<FaStar key={i} className="inline-block text-red-900" />);
-        } else if (rating === 2) {
-          stars.push(<FaStar key={i} className="inline-block text-red-500" />);
-        } else if (rating === 3) {
-          stars.push(
-            <FaStar key={i} className="inline-block text-green-500" />
-          );
-        } else if (rating === 4) {
-          stars.push(
-            <FaStar key={i} className="inline-block text-indigo-500" />
-          );
-        } else {
-          stars.push(
-            <FaStar key={i} className="inline-block text-indigo-900" />
-          );
-        }
-      } else {
-        stars.push(<FaStar key={i} className="inline-block text-gray-300" />);
+        stars.push(<FaStar key={i} className="inline-block text-yellow-300" />);
       }
     }
     return stars;
   };
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    pauseOnHover: true,
-  };
-  return (
-    <div className="p-20">
-      <p className="text-3xl text-transparent  text-center font-bold py-4 bg-clip-text bg-gradient-to-r from-indigo-700 to-red-500 uppercase font-[VT323] ">
-        {" "}
-        What Our Clients Say
-      </p>
 
-      <Slider {...settings} className="mr-0 ml-0 lg:ml-20 pb-4 ">
+  return (
+    <div className="bg-white mx-[45px] md:mx-[130px] py-20 text-center ">
+      <h6 className="tracking-widest py-4">TESTIMONIALS</h6>
+      <h1 className=" text-xl md:text-5xl font-bold pb-10 text-transparent  text-center  py-4 bg-clip-text bg-gradient-to-r from-indigo-700 to-red-500 uppercase">
+        Our Customers
+      </h1>
+      <div className="flex items-center justify-center pb-6">
+        <Lottie animationData={star} className=" w-20 md:w-48" />
+      </div>
+
+      <Swiper
+        spaceBetween={40}
+        slidesPerView={2}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          "@0.75": {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          "@1.00": {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          "@1.50": {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+        }}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
         {reviews?.map((review: ReviewProps) => {
           return (
-            <div key={review._id} className="ml-0 lg:ml-36">
-              <div className="relative my-5">
-                <Image
-                  src={"/chat-4.png"}
-                  alt="review image"
-                  width={350}
-                  height={300}
-                  className="w-[400px] h-[400px]"
-                />
-                <div className="absolute  top-[20%] left-[5%] px-6 lg:px-0 w-[230px]  lg:w-[300px]">
-                  <p className="text-base lg:text-lg  font-bold  text-black p-2  font-[VT323] ">
-                    <span className="lg:text-2xl text-base ">&rsquo;</span>{" "}
-                    {review.comment}
-                  </p>
-
-                  <div className="flex items-end justify-center  text-xs lg:text-base">
-                    {renderRatingStars(review.rating)}
+            <SwiperSlide>
+              <div className="contailer  flex flex-col items-start">
+                <div className="my-6">
+                  <div className="flex justify-start items-center gap-2 ">
+                    {review?.imgUrl ? (
+                      <div className=" ">
+                        <Image
+                          src={review.imgUrl}
+                          alt="User photo"
+                          width={20}
+                          height={20}
+                          className="w-[20px] h-[20px] lg:w-[40px] lg:h-[40px] rounded-full"
+                        />
+                      </div>
+                    ) : (
+                      <div>
+                        <CgProfile className="lg:w-10 lg:h-10 w-5 h-5 " />
+                      </div>
+                    )}
+                    <h1 className=" text-sm text-black ">{review.name}</h1>
                   </div>
+                  <p className="  text-gray-900  text-xs">{review.email}</p>
                 </div>
+                <p className="text-sm font-light text-black text-start">
+                  {review.comment}
+                </p>
+                <p className="flex items-center justify-cneter mt-1">
+                  {" "}
+                  {renderRatingStars(review.rating)}
+                </p>
               </div>
-              <div className="flex flex-col justify-start items-start -mt-10">
-                <div className="flex justify-center items-center gap-2 ">
-                  {review?.imgUrl ? (
-                    <div className=" ">
-                      <Image
-                        src={review.imgUrl}
-                        alt="User photo"
-                        width={20}
-                        height={20}
-                        className="w-[20px] h-[20px] lg:w-[40px] lg:h-[40px] rounded-full"
-                      />
-                    </div>
-                  ) : (
-                    <div>
-                      <CgProfile className="lg:w-10 lg:h-10 w-5 h-5 " />
-                    </div>
-                  )}
-
-                  <div className="flex flex-col justify-center ">
-                    <p className="text-black font-bold text-sm lg:text-lg lg:font-[VT323]">
-                      {review.name}
-                    </p>
-                    <p className="text-xs text-gray-500">{review.email}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </Slider>
+      </Swiper>
     </div>
   );
 };
